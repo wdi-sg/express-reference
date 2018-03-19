a3a1fd2ce180896eb70143830fcfbb0dc6a074dc
awongh <akira@awongh.com>
2018-03-19 21:56:52 +0800

    working sql example

--- index.js
+++ index.js
@@ -4,8 +4,22 @@ const methodOverride = require('method-override')
 const exphbs = require('express-handlebars');
 const jsonfile = require('jsonfile');
 
+const { Client } = require('pg')
+
+const client = new Client({
+  user: 'akira',
+  host: '127.0.0.1',
+  database: 'pokemons',
+  port: 5432,
+});
+
 // const jsonfile = require('jsonfile');
 const FILE = 'data.json';
+
+
+// how to init db
+//createdb pokemons -U akira
+//psql -U akira -d pokemons -a -f seed.sql
 
 /**
  * ===================================
@@ -23,6 +37,7 @@ app.use(methodOverride('_method'))
 // Set handlebars to be the default view engine
 app.engine('handlebars', exphbs.create().engine);
 app.set('view engine', 'handlebars');
+
 
 /**
  * ===================================
@@ -45,24 +60,32 @@ app.get('/pokemon/new', (req, res) => {
   res.render('new');
 });
 
-app.post('/pokemon', (req, res) => {
+app.post('/pokemon', (req, response) => {
   let params = req.body;
 
-  let pokemon = {
-    name:params.name,
-    height:params.height,
-  };
+  const text = 'INSERT INTO pokemon(name, height) VALUES($1, $2)'
+  const values = [params.name, params.height];
 
-  jsonfile.readFile(FILE, (err, obj) => {
+  client.connect((err) => {
 
-    obj["pokemon"].push( pokemon );
+    //TODO: check for connection error
 
-    jsonfile.writeFile(FILE, obj, (err) => {
-      console.error(err)
+    client.query(text, values, (err, res) => {
+      if (err) {
+        console.log(err.stack);
+      } else {
+        console.log(res.rows[0]);
+        // { name: 'brianc', email: 'brian.m.carlson@gmail.com' }
+        // render the id's page?
+        // res.render('pokemon');
+        // or redirect
+        response.render('home');
+
+      }
     });
+
   });
 
-  res.render('home');
 });
 
 
