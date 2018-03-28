1a6c069e987f6a8e95feb84d1964eac5078b9ebe
awongh <akira@awongh.com>
2018-03-28 19:12:27 +0800

    heroku deploy changes

--- db.js
+++ db.js
@@ -2,12 +2,31 @@ const pg = require('pg');
 const pokemon = require('./models/pokemon');
 const user = require('./models/user');
 
-const configs = {
-  user: 'postgres',
-  host: '127.0.0.1',
-  database: 'pokemons',
-  port: 5432
-};
+var configs;
+
+if( process.env.DATABASE_URL ){
+
+  const params = url.parse(process.env.DATABASE_URL);
+  const auth = params.auth.split(':');
+
+  configs = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    ssl: true
+  };
+
+else{
+  configs = {
+    user: 'postgres',
+    host: '127.0.0.1',
+    database: 'pokemons',
+    port: 5432
+  };
+}
+
 
 const pool = new pg.Pool(configs);
 
