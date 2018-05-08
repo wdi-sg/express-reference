ffb070024591ba68f744ba92e5942b4462eb840d
Ray Tham <RayWTham@gmail.com>
2018-05-08 15:23:55 +0800

    removed user

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require("express");
-const bcrypt = require('bcrypt');
 const jsonfile = require("jsonfile");
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 const cookieParser = require("cookie-parser");
 const FILE = "pokedex.json";
-const USERFILE = "users.json"
 
 /**
  * ===================================
@@ -37,60 +35,6 @@ app.use(methodOverride("_method"));
  * Routes
  * ===================================
  */
-
-app.get("/users/new", (request, response) => {
-  // send response with some data (a HTML file)
-  response.render("newuser");
-});
-
-app.post("/users/new", (request, response) => {
-  jsonfile.readFile(USERFILE, (err, obj) => {
-    bcrypt.hash(request.body.password, 1, function(err, hash) {
-      let new_user = {
-        name: request.body.name,
-        password: hash
-      };
-
-      obj.users.push(new_user);
-
-      jsonfile.writeFile(USERFILE, obj, err => {
-        console.error(err);
-
-        response.cookie("logged_in", true);
-        response.redirect(301, "/");
-      });
-    });
-  });
-});
-
-app.post("/users/logout", (request, response) => {
-  response.clearCookie("logged_in");
-  response.redirect(301, "/");
-});
-
-app.get("/users/login", (request, response) => {
-  // send response with some data (a HTML file)
-  response.render("Login");
-});
-
-app.post("/users/login", (request, response) => {
-  jsonfile.readFile(USERFILE, (err, obj) => {
-    let user = null;
-
-    for (let i = 0; i < obj.users.length; i++) {
-      if (obj.users[i].name == request.body.name) {
-        user = obj.users[i];
-      }
-    }
-
-    bcrypt.compare(request.body.password, user.password, function(err, res) {
-      if (res === true) {
-        response.cookie("logged_in", true);
-        response.redirect(301, "/");
-      }
-    });
-  });
-});
 
 app.get("/edit/:id", (request, response) => {
   // send response with some data (a HTML file)
@@ -210,8 +154,7 @@ app.get("/:id", (request, response) => {
 });
 
 app.get("/", (request, response) => {
-  let visits = request.cookies["visits"];
-  let logged_in = request.cookies["logged_in"];
+  var visits = request.cookies["visits"];
 
   if (visits === undefined) {
     visits = 1;
@@ -222,11 +165,10 @@ app.get("/", (request, response) => {
   response.cookie("visits", visits);
 
   let context = {
-    visits: visits,
-    logged_in: logged_in
+    visits: visits
   };
 
-  response.render("Home", context);
+  response.render("home", context);
 });
 
 /**
